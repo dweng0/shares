8c431b38d69fe87df0627c2891b4325e7a5671f7
Jay <jmartinsmith@outlook.com>
2022-12-22 11:20:02 +0000

    added upstream tests

---
+++ src/controller/__tests__/upstream.test.ts
@@ -0,0 +1,32 @@
+import { Payment } from '../../models/payment';
+import { byBankTransfers, byPendingPayment } from '../../services/helpers';
+import { MOCK_PAYMENT_PARTIAL } from '../../__mocks__/test.mocks';
+import { getBankTransferResult, getBankTransferResults } from '../upstream';
+
+describe("Tests for grabbing data from upstream", () => {
+    it("Should get the payment result from upstream", async () => {
+        // setup 
+        const bankPayments = MOCK_PAYMENT_PARTIAL
+                                .filter(byBankTransfers)
+                                .filter(byPendingPayment)
+        // execute
+        const result = await getBankTransferResult(bankPayments[0] as Payment);
+
+        // verify
+        expect(result.transactionDetails.bankTransfer.status).toBe('processed');
+
+
+    });
+
+    it("Should get the array payment results from upstream", async () => {
+        // setup 
+        const bankPayments = MOCK_PAYMENT_PARTIAL
+                .filter(byBankTransfers)
+                .filter(byPendingPayment)
+        // execute
+        const result = await getBankTransferResults(bankPayments as Payment[]);
+
+        // verify
+        expect(result.every(payment => payment.transactionDetails.bankTransfer.status === 'processed')).toBe(true);
+    });
+});
